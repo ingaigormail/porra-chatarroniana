db1277d1ce9e4eda8277253efd4d3800be752469
ingaigormail <inga.igor.mail@gmail.com>
2026-06-13 21:24:02 +0200

    Remove unused import for Credentials
    Removed unused import for Credentials from Google OAuth2.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import gspread
-from google.oauth2.service_account import Credentials
+# en teoria lo borro from google.oauth2.service_account import Credentials
 import flag
 from collections import Counter
 import plotly.graph_objects as go
